af29ed78152fd6ccca4d8c6cedc268f07c3867bc
hendrakrnm <hendrakurnia421@gmail.com>
2025-08-02 04:45:51 +0700

    FOOTER: link sosmed + show Landing Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,11 @@
 
 import React from "react";
-
+import LandingPage from "@/app/mainPage/page"
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-      
-      <div className="pt-100">
-
-        <h1>Hendra ganteng Banget</h1>
-      </div>
-      
+    <div>
+      <LandingPage />
     </div>
   );
 }
