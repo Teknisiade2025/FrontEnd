d0242e2b07fa7b215ace50628f292c132f454496
maritzavaniaa <maritzavaniadellia@gmail.com>
2025-08-09 20:45:28 +0700

    ADMIN SCOREBOARD & ADMIN MEDAL: buat komponen dan page

---
+++ src/app/adminMedal/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MedalManage from "../component/admin/klasemen/medalManage.jsx";
+
+const MedalPage = () => {
+  return <MedalManage />;
+};
+
+export default MedalPage;
---
+++ src/app/adminScoreboard/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Scoreboard from "../component/admin/scoreboard/scoreboard.jsx";
+
+const ScoreboardPage = () => {
+  return <Scoreboard />;
+};
+
+export default ScoreboardPage;
