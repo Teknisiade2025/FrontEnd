cb6b1fed9720a046211baa44cd850824faeebbe4
Fanny Elisabeth <fannyelisabethpanjaitan3003@mail.ugm.ac.id>
2025-08-15 16:44:01 +0700

    kedua

--- src/app/managerDashboard/page.jsx
+++ src/app/managerDashboard/page.jsx
@@ -2,6 +2,7 @@
 import Dashboard from '@/app/component/admin/dashboard/managerDashboard';
 import React, { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function HomePage() {
   const searchParams = useSearchParams();  
@@ -37,9 +38,13 @@ export default function HomePage() {
 
   return (
     <div className="flex flex-row min-h-screen pt-10 gap-1 w-screen bg-[url('/bgdashboard.svg')] bg-cover bg-no-repeat bg-center">
-      <div className="flex-1 pt-8">
+       <Suspense fallback={<div className="pt-5 text-white">Loading...</div>}>
+       <div className="flex-1 pt-8">
         <Dashboard kmhmName={kmhmName} goToAthletes={goToAthletes} />
       </div>
+          
+        </Suspense>
+      
     </div>
   );
 }
